66ae57b8c55b99d0dd58e562f63049be2396f38a
alinazarakovska <alinazarakovsaa@gmail.com>
2021-01-11 13:43:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#mainīgie 
+a = 5 
+print(a)
+print(a+a)
+a = a + a            #'=' piešķir vērtību.
+print(a)
+print(type(a))
+a = 30.1
+print(type(a))
+
+#Nodokļi
+income = 100 
+nodoklis = 0.1       #jeb 10%
+MansNodoklis = income * nodoklis 
+print(MansNodoklis)
+
+#Datu ievade 
+x = input("ievadi savu vārdu :")
+print("Tavs ievadīts vārds ir " + x)
